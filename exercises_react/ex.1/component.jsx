ef6c6b18a94d49e529471e048a51ce0b425081d7
adoniasvsbarros <adoniasvicente@hotmail.com>
2019-04-03 21:31:26 -0300

    Adding first react exercises

---
+++ exercises_react/ex.1/component.jsx
@@ -0,0 +1,23 @@
+// First component in react
+
+import React from 'react'
+
+/*
+export default function(){
+    return <h1>First component</h1>
+}
+*/
+
+/*
+export default () => {
+    return <h1>First component</h1>
+}
+*/
+
+/*
+export default () => <h1>First component</h1>
+*/
+
+export default () => (
+    <h1>First component</h1>
+)
---
+++ exercises_react/ex.2/component.jsx
@@ -0,0 +1,7 @@
+// First component in react
+
+import React from 'react'
+
+export default (props) => (
+    <h1>{props.value}</h1>
+)

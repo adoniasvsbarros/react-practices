d6137a001513bd08b6ddfa6b040a0b6833158bac
adoniasvsbarros <adoniasvicente@hotmail.com>
2019-04-23 22:00:53 -0300

    adding new react exercises with create-react-app command

---
+++ exercises_react_new/src/components/FirstComponent.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function first(){
+    return <h1>First component!</h1>
+}
+
+export default first
---
+++ exercises_react_new/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import FirstComponent from './components/FirstComponent'
+
+const element = document.getElementById('root')
+
+ReactDOM.render(
+    <div>
+        <FirstComponent></FirstComponent>
+    </div>
+    , element)
+
+
+// const jsx = <h1>Hello react!</h1>

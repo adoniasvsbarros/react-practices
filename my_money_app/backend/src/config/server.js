a7cff33f87d138aca63baf4858a770e6fe85b609
adoniasvsbarros <adoniasvicente@hotmail.com>
2019-05-17 00:40:35 -0300

    billingCycle allowing cors

--- my_money_app/backend/src/config/server.js
+++ my_money_app/backend/src/config/server.js
@@ -5,10 +5,12 @@ const port = 3003
 const bodyParser = require('body-parser')
 const express = require('express')
 const server = express()
+const allowCors = require('./cors')
 
 // middleware
 server.use(bodyParser.urlencoded({extended: true}))
 server.use(bodyParser.json())
+server.use(allowCors)
 
 server.listen(port, function(){
     console.log(`Backend is running on port ${port}`)

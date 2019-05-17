56bec703267d8a5a6b1757f4b49465b6b95ac635
adoniasvsbarros <adoniasvicente@hotmail.com>
2019-05-17 00:46:05 -0300

    billingCycle middleware parse request string to int

--- my_money_app/backend/src/config/server.js
+++ my_money_app/backend/src/config/server.js
@@ -6,11 +6,13 @@ const bodyParser = require('body-parser')
 const express = require('express')
 const server = express()
 const allowCors = require('./cors')
+const queryParser = require('express-query-int')
 
-// middleware
+// middlewares
 server.use(bodyParser.urlencoded({extended: true}))
 server.use(bodyParser.json())
 server.use(allowCors)
+server.use(queryParser())
 
 server.listen(port, function(){
     console.log(`Backend is running on port ${port}`)

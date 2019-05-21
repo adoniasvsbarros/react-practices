642708580230dc81233a840cd1a3ae0428761f79
adoniasvsbarros <adoniasvicente@hotmail.com>
2019-05-21 11:11:14 -0300

    my money app, integrating dashboard with redux

--- my_money_app/frontend/src/dashboard/dashboard.jsx
+++ my_money_app/frontend/src/dashboard/dashboard.jsx
@@ -1,19 +1,45 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
 
+import { getSummary } from './dashboardActions'
 import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
+import ValueBox from '../common/widget/valueBox'
+import Row from '../common/layout/row'
+
+
 
 class Dashboard extends React.Component {
+    
+    componentWillMount(){
+        this.props.getSummary()
+    }
+
     render(){
+        const { credit, debit } = this.props.summary
         return(
             <div>
                 <ContentHeader title='Dashboard' small='Version 1.0' />
                 <Content>
-                    Dashboard
+                    <Row>
+                        <ValueBox cols='12 4' color='green' icon='bank'
+                            value={`R$ ${credit}`} text='Total Credits' />
+                        <ValueBox cols='12 4' color='red' icon='credit-card'
+                            value={`R$ ${debit}`} text='Total Debits' />
+                        <ValueBox cols='12 4' color='blue' icon='money'
+                            value={`R$ ${credit - debit}`} text='Consolidated value' />
+                    </Row>
                 </Content>
             </div>
         )
     }
 }
 
-export default Dashboard
+const mapStateToProps = state => ({
+    summary : state.dashboard.summary 
+})
+
+const mapDispatchToProps = dispatch => bindActionCreators({getSummary}, dispatch)
+
+export default connect(mapStateToProps, mapDispatchToProps)(Dashboard)

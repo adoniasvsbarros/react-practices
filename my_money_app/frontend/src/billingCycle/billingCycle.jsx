d8011c0903ba0da7fc78496bec165f28b0b11638
adoniasvsbarros <adoniasvicente@hotmail.com>
2019-05-21 11:53:05 -0300

    my money app, refactoring billing cycle component

--- my_money_app/frontend/src/billingCycle/billingCycle.jsx
+++ my_money_app/frontend/src/billingCycle/billingCycle.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
 
-export default props => (
-    <div>
-        <h1>Billing Cycles</h1>
-    </div>
-)
+import ContentHeader from '../common/template/contentHeader'
+import Content from '../common/template/content'
+
+class BillingCycle extends React.Component{
+    render(){
+        return(
+            <div>
+                <ContentHeader title='Billing Cycles' small='Registry' />
+                <Content>
+                    Billing Cycles 
+                </Content>
+            </div>
+        )
+    }
+} 
+
+export default BillingCycle;

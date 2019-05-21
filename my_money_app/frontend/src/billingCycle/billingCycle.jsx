ec5189cf953f54faa4eeb73d43c0dd3fe78a1e8d
adoniasvsbarros <adoniasvicente@hotmail.com>
2019-05-21 12:09:22 -0300

    my money app, creating tabheader component

--- my_money_app/frontend/src/billingCycle/billingCycle.jsx
+++ my_money_app/frontend/src/billingCycle/billingCycle.jsx
@@ -5,7 +5,7 @@ import Content from '../common/template/content'
 import Tabs from '../common/tab/tabs'
 import TabsHeader from '../common/tab/tabsHeader'
 import TabsContent from '../common/tab/tabsContent'
-
+import TabHeader from '../common/tab/tabHeader'
 
 class BillingCycle extends React.Component{
     render(){
@@ -15,10 +15,13 @@ class BillingCycle extends React.Component{
                 <Content>
                     <Tabs>
                         <TabsHeader>
-
+                            <TabHeader label='List' icon='bars' target='tabList' />
+                            <TabHeader label='Create' icon='plus' target='tabCreate' />
+                            <TabHeader label='Update' icon='pencil' target='tabUpdate' />
+                            <TabHeader label='Delete' icon='trash-o' target='tabDelete' />
                         </TabsHeader>
                         <TabsContent>
-                            
+
                         </TabsContent>
 
                     </Tabs>

3e2f4f9987ad94e22cefa23c85501168ff994005
adoniasvsbarros <adoniasvicente@hotmail.com>
2019-05-30 12:14:11 -0300

    my money app, authentication backend

--- my_money_app/backend/src/config/routes.js
+++ my_money_app/backend/src/config/routes.js
@@ -1,13 +1,23 @@
 const express = require('express')
+const auth = require('./auth')
 
-module.exports = function(server){
-    // define base URL for all routes
-    const router = express.Router()
-    server.use('/api', router)
-
-    // billingCycle routes mapping
-    const billingCycle = require('../api/billingCycle/billingCycleService')
-    // register all webservices rest inside /billingCycles route
-    billingCycle.register(router, '/billingCycles')
-
+module.exports = function (server) {
+    /*
+    * Rotas protegidas por Token JWT
+    */
+    const protectedApi = express.Router()
+    server.use('/api', protectedApi)
+    protectedApi.use(auth)
+    const BillingCycle = require('../api/billingCycle/billingCycleService')
+    BillingCycle.register(protectedApi, '/billingCycles')
+    
+    /*
+    * Rotas abertas
+    */
+    const openApi = express.Router()
+    server.use('/oapi', openApi)
+    const AuthService = require('../api/user/authService')
+    openApi.post('/login', AuthService.login)
+    openApi.post('/signup', AuthService.signup)
+    openApi.post('/validateToken', AuthService.validateToken)
 }

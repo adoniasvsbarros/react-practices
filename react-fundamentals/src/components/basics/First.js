544f8504184e63985bd5c4b29c3daca0b3224651
developer <adoniasvicente@hotmail.com>
2020-07-13 12:15:42 -0400

    improving first component

--- react-fundamentals/src/components/basics/First.js
+++ react-fundamentals/src/components/basics/First.js
@@ -1,5 +1,13 @@
-function First() {
-    return "First Component";
+import React from 'react';
+
+export default function First() {
+    const msg = "Welcome back!";
+
+    return (
+        <div>
+            <h2>First Component</h2>
+            <p>{ msg }</p>
+        </div>
+    );
 }
 
-export default First;

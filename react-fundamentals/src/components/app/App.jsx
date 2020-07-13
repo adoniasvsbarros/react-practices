6f8d0ee98990879fff674be5592f3370799a63b8
developer <adoniasvicente@hotmail.com>
2020-07-13 14:24:46 -0400

    create card component

--- react-fundamentals/src/components/app/App.jsx
+++ react-fundamentals/src/components/app/App.jsx
@@ -1,21 +1,38 @@
 import React from "react";
 
 import "../../index.css";
+import "./App.css";
 
 import ComponentWithoutParameter from "../componentWithoutParameter/ComponentWihtoutParameter";
 import ComponentWithParameter from "../componentWithParameter/ComponentWithParameter";
 import Fragment from "../Fragment";
-import Random from '../random/Random';
+import Random from "../random/Random";
+import Card from "../card/Card";
 
 export default (_) => (
-  <div id="app">
-    <Random min={5} max={15} />
-    <Fragment />
-    <ComponentWithParameter
-      title="Student Situation"
-      student="Paul"
-      grade={6.9}
-    />
-    <ComponentWithoutParameter />
+  <div className="App">
+    <h1>React Fundamentals</h1>
+
+    <div className="Cards">
+      <Card title="#04 - Random Challenge">
+        <Random min={5} max={15} />
+      </Card>
+
+      <Card title="#03 - Fragment">
+        <Fragment />
+      </Card>
+
+      <Card title="#02 - Component with parameter">
+        <ComponentWithParameter
+          title="Student Situation"
+          student="Paul"
+          grade={6.9}
+        />
+      </Card>
+
+      <Card title="#01 - Component without Parameter">
+        <ComponentWithoutParameter />
+      </Card>
+    </div>
   </div>
 );
